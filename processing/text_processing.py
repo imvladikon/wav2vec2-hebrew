9b691b26020e647703a990332dbff1b3fc9cba04
Vladimir Gurevich <imvladikon@gmail.com>
2022-01-25 18:27:49 +0200

    init

--- processing/text_processing.py
+++ processing/text_processing.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+SOFIT_MAP = {
+    'ך': 'כ',
+    'ם': 'מ',
+    'ן': 'נ',
+    'ף': 'פ',
+    'ץ': 'צ',
+}
+SOFIT_MAP_INV = {v: k for k, v in SOFIT_MAP.items()}
+
 
 def get_alephbet():
     return "".join(chr(i) for i in range(ord('א'), ord('ת') + 1))
 
+
 def remove_sofits(text):
-    return
+    return text.translate(str.maketrans(SOFIT_MAP))
+
 
 def return_sofits(text):
-    return
+    tokens = text.split()
+    for idx, t in enumerate(tokens):
+        if t[-1] in SOFIT_MAP_INV:
+            tokens[idx] = t[:-1] + SOFIT_MAP_INV[t[-1]]
+    return " ".join(tokens)
 
 
 if __name__ == '__main__':
     print(get_alephbet())
+    print(remove_sofits("ךםןףץ"))
+    print(return_sofits("ממ ננ פפ צצ"))
